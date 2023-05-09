87de690b4ab7687b7058bc02e0665e5b02146112
JoaoSts <joaosantos.js234@gmail.com>
2023-05-09 11:55:20 -0300

    #terminei a pagina de cursos, fiz um js pro modal,add um faicon no index e no cursos.

---
+++ script/cursosPag.js
@@ -0,0 +1,39 @@
+document.getElementById("btn-curso1").addEventListener("click",function Modal(){
+    var img = document.getElementById("img-curso")
+    var h2 = document.getElementById("title-curso")
+    var p = document.getElementById("p-curso")
+    var btnCurso = document.getElementById("btn-curso")
+    
+    
+       
+  img.src="./IMG/ilustra-poupando.png"
+  img.style.border='1px solid #419488'
+  h2.textContent='Investimento para iniciantes'
+  
+  
+    
+  });
+  document.getElementById("btn-curso2").addEventListener("click",function Modal2(){
+    var img = document.getElementById("img-curso")
+    var h2 = document.getElementById("title-curso")
+    var p = document.getElementById("p-curso")
+    var btnCurso = document.getElementById("btn-curso")
+   
+    
+    h2.textContent='Poupando e Rendendo'
+       img.src="./IMG/ilustra-investimento.png"
+    
+  });
+  document.getElementById("btn-curso3").addEventListener("click",function Modal3(){
+    var img = document.getElementById("img-curso")
+    var h2 = document.getElementById("title-curso")
+    var p = document.getElementById("p-curso")
+    var btnCurso = document.getElementById("btn-curso")
+    h2.textContent='Independência Financeira'
+    img.src="./img/ilustra-independencia.png"
+    img.style.width="38%"
+       
+    
+  });
+  
+  

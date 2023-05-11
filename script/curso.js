dbe949391f2c0b9efc26130f792805dd6a1138b9
JoaoSts <joaosantos.js234@gmail.com>
2023-05-10 21:09:50 -0300

    #criação de interação entre paginas e atualização de prompt do  curso, e atualização de página de curso

---
+++ script/curso.js
@@ -0,0 +1,22 @@
+
+var id = window.location.href;
+var img = document.getElementById("img-curso")
+var curso = document.getElementById("curso-h1")
+
+function Pegacurso(){
+
+    if (id.includes("id=1")) {
+        img.src = "./IMG/ilustra-poupando.png"
+        curso.textContent = 'Investimento para iniciantes'
+    } else if (id.includes("id=2")) {
+        img.src="./IMG/ilustra-investimento.png"
+        curso.textContent = 'Poupando e Rendendo'
+    } else {
+        img.src="./img/ilustra-independencia.png"
+        curso.textContent = 'Independência Financeira'
+    }
+    
+}
+
+
+Pegacurso()
--- script/cursosPag.js
+++ script/cursosPag.js
@@ -2,13 +2,20 @@ document.getElementById("btn-curso1").addEventListener("click",function Modal(){
     var img = document.getElementById("img-curso")
     var h2 = document.getElementById("title-curso")
     var p = document.getElementById("p-curso")
-    var btnCurso = document.getElementById("btn-curso")
+    var btnCurso = document.getElementById("link")
     
     
        
   img.src="./IMG/ilustra-poupando.png"
   img.style.border='1px solid #419488'
   h2.textContent='Investimento para iniciantes'
+  btnCurso.addEventListener("click",()=>{
+    btnCurso.href='./curso.html'
+    btnCurso.target="_blank"
+    window.location.href='./curso.html'+"?id="+1
+    
+    
+  })
   
   
     
@@ -17,21 +24,34 @@ document.getElementById("btn-curso1").addEventListener("click",function Modal(){
     var img = document.getElementById("img-curso")
     var h2 = document.getElementById("title-curso")
     var p = document.getElementById("p-curso")
-    var btnCurso = document.getElementById("btn-curso")
+    var btnCurso = document.getElementById("link")
    
     
     h2.textContent='Poupando e Rendendo'
        img.src="./IMG/ilustra-investimento.png"
+       btnCurso.addEventListener("click",()=>{
+        btnCurso.href='./curso.html'
+        btnCurso.target="_blank"
+        window.location.href='./curso.html'+"?id="+2
+      })
     
   });
   document.getElementById("btn-curso3").addEventListener("click",function Modal3(){
     var img = document.getElementById("img-curso")
     var h2 = document.getElementById("title-curso")
     var p = document.getElementById("p-curso")
-    var btnCurso = document.getElementById("btn-curso")
+    var btnCurso = document.getElementById("link")
+
+
     h2.textContent='Independência Financeira'
     img.src="./img/ilustra-independencia.png"
     img.style.width="38%"
+
+    btnCurso.addEventListener("click",()=>{
+      btnCurso.href='./curso.html'
+      btnCurso.target="_blank"
+      window.location.href='./curso.html'+"?id="+3
+    })
        
     
   });
